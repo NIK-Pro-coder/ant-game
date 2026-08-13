2586a925ac2c22102d3066eebcab0533174b75bb
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2026-08-13 16:50:35 +0200

    Added stat holder resource, holy I hate how I made this

--- components/stats/stat_comp.gd
+++ components/stats/stat_comp.gd
@@ -1,1 +1,62 @@
 class_name StatComp extends Node
+
+var spd_holder := StatHolder.new()
+var atk_holder := StatHolder.new()
+var def_holder := StatHolder.new()
+
+#region UGLY AHH CODE
+@export var spd_base: float = 0.0:
+  set(value):
+    spd_base = value
+    spd_holder.stat_base = value
+var spd_mult: float = 1.0:
+  set(value):
+    spd_mult = value
+    spd_holder.stat_mult = value
+var spd_flat: float = 0.0:
+  set(value):
+    spd_flat = value
+    spd_holder.stat_flat = value
+var spd: float = 0.0:
+  set(value): pass # make ts readonly
+  get(): return spd_holder.stat_value
+
+@export var atk_base: float = 0.0:
+  set(value):
+    atk_base = value
+    atk_holder.stat_base = value
+var atk_mult: float = 1.0:
+  set(value):
+    atk_mult = value
+    atk_holder.stat_mult = value
+var atk_flat: float = 0.0:
+  set(value):
+    atk_flat = value
+    atk_holder.stat_flat = value
+var atk: float = 0.0:
+  set(value): pass # make ts readonly
+  get(): return atk_holder.stat_value
+
+@export var def_base: float = 0.0:
+  set(value):
+    def_base = value
+    def_holder.stat_base = value
+var def_mult: float = 1.0:
+  set(value):
+    def_mult = value
+    def_holder.stat_mult = value
+var def_flat: float = 0.0:
+  set(value):
+    def_flat = value
+    def_holder.stat_flat = value
+var def: float = 0.0:
+  set(value): pass # make ts readonly
+  get(): return def_holder.stat_value
+#endregion
+
+signal stats_changed
+
+func _ready() -> void:
+  spd_holder.changed.connect(stats_changed.emit)
+  atk_holder.changed.connect(stats_changed.emit)
+  def_holder.changed.connect(stats_changed.emit)
--- components/stats/stat_holder.gd
+++ components/stats/stat_holder.gd
@@ -1,9 +1,28 @@
 class_name StatHolder extends Resource
 
-@export var stat_base: float = 125.0
-@export var stat_flat: float = 0.0
-@export var stat_mult: float = 1.0
+@export var stat_base: float = 0.0:
+  set(value):
+    if value == stat_base: return
+    
+    stat_base = value
+    stat_changed.emit()
+
+@export var stat_flat: float = 0.0:
+  set(value):
+    if value == stat_flat: return
+    
+    stat_flat = value
+    stat_changed.emit()
+
+@export var stat_mult: float = 1.0:
+  set(value):
+    if value == stat_mult: return
+    
+    stat_mult = value
+    stat_changed.emit()
 
 var stat_value: float = 0.0:
   set(value): pass # make ts readonly
   get(): return stat_base * stat_mult + stat_flat
+
+signal stat_changed
